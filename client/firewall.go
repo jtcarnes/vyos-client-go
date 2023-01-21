df71541aea9d49c9cb82331a1512e3e342e88593
jtcarnes <56089764+jtcarnes@users.noreply.github.com>
2023-01-21 20:07:03 +0000

    adding initial firewall rules

---
+++ client/firewall.go
@@ -0,0 +1,6 @@
+package client
+
+type FirewallConfig struct {
+	AllPing       bool `json:"ping-all"`
+	BroadcastPing bool `json:"bread`
+}
---
+++ client/firewallk_test.go
@@ -0,0 +1,7 @@
+package client_test
+
+import "testing"
+
+func TestBasic_Firewall_Read(t *testing.T) {
+
+}
